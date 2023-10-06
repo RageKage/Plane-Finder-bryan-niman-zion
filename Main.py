fc6b4d9900f968a882a6c8a5b725c93e72d860c4
ju957 <ju9572fp@go.minnstate.edu>
2023-10-06 00:52:19 -0500

    Add menu class and sample menu ui

--- Main.py
+++ Main.py
@@ -1,9 +1,50 @@
-
+import databaseManager
+from databaseManager import Plane
+from menu import Menu
 
 def Main():
-    print('This works!')
         # Create a menu here.
+    menu = create_menu()
 
 
+    while True:
+        print(menu)
+        choice = input('Enter action here: ')
+        action = menu.get_choice(choice)
+        if action is None:
+            exit()
+        action()
+
+
+# this function creates the menu to be seen by the user.
+def create_menu():
+    menu = Menu()
+    menu.add_option('1', 'Show all bookmarked planes', show_bookmarked_planes)
+    menu.add_option('2', 'Search for a plane', plane_search)
+    menu.add_option('3', 'Bookmark a plane', bookmark_plane)
+    menu.add_option('4', 'Quit program', exit)
+
+    return menu
+
+
+def plane_search():
+    print('Currently nonfunctional')
+
+
+def bookmark_plane():
+    print('Currently nonfunctional')
+    # To be added
+
+
+# Shows all bookmarked planes
+def show_bookmarked_planes():
+    planes = databaseManager.get_planes()
+    print()
+    for plane in planes:
+        print(plane)
+    print()
+
+
+# Runs the program
 if __name__ == '__main__':
     Main()
---
+++ menu.py
@@ -0,0 +1,28 @@
+class Menu:
+
+    """This class is used to create and access a menu in the main program. Menu options are selected by a one character
+       entry (longer entries are possible though), and options are shown by printing the menu after options have been
+       added to it."""
+
+    # Two dictionaries are created so that options can be looped through and still use normal function names.
+    def __init__(self):
+        self.option_descriptions = {}
+        self.functions = {}
+
+    # Adds an option to the menu. the key is the character the user will need to press to select said option, the
+    # description should be a string explanation for what the option does, and the function parameter calls a desired
+    # function when the option is selected. The function parameter isn't shown to the user.
+    def add_option(self, key, description, function):
+        self.option_descriptions[key] = description
+        self.functions[key] = function
+
+    # Returns what function should be invoked if one is found. If nothing is found, NoneType is returned.
+    def get_choice(self, choice):
+        return self.functions.get(choice)
+
+    # Creates a printable menu with each option on their own line.
+    def __str__(self):
+        menu_print = ''
+        for key in self.option_descriptions.keys():
+            menu_print += f'{key}: {self.option_descriptions[key]}\n'
+        return menu_print
