20fe2069cb4051d1ceb7d4657acc3a91c1d3127b
ju957 <ju9572fp@go.minnstate.edu>
2023-10-06 00:53:08 -0500

    Create a database with peewee

--- databaseManager.py
+++ databaseManager.py
@@ -1,6 +1,8 @@
 from peewee import *
 
+# Uses the database called planes.sqlite in the database folder, or creates one if it can't be found.
 db = SqliteDatabase('database\planes.sqlite')
+
 
 class Plane(Model):
     name = CharField()
@@ -11,6 +13,11 @@ class Plane(Model):
 
     def __str__(self):
         return f'{self.name} is {self.description}'
+
+
+# Returns all bookmarked planes using peewee to access the database
+def get_planes():
+    return Plane.select()
 
 db.connect()
 db.create_tables([Plane])
