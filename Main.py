b96aa2b564cd63f026449e69b1e428a652679668
RageKage <99547837+RageKage@users.noreply.github.com>
2023-10-12 07:38:14 -0500

    add more docs to dbManger and updated vari names

--- Main.py
+++ Main.py
@@ -20,19 +20,19 @@ def Main():
 # this function creates the menu to be seen by the user.
 def create_menu():
     menu = Menu()
-    menu.add_option('1', 'DB Plane Search', plane_search)
-    menu.add_option('2', 'API Plane Search', search_plane)
-    menu.add_option('3', 'Display Bookmarked Planes', get_show_bookmarked_planes)
+    menu.add_option('1', 'DB Plane Search', search_bookmarked_plane)
+    menu.add_option('2', 'API Plane Search', search_aircraft_in_api)
+    menu.add_option('3', 'Display Bookmarked Planes', display_bookmarked_planes)
     
     # menu.add_option('4', 'Bookmark a Plane', bookmark_plane) possible option, TBD whether this is worth it.
     
-    menu.add_option('4', 'Delete Plane in DB', delete_plane)
+    menu.add_option('4', 'Delete Plane in DB', delete_bookmarked_plane)
     menu.add_option('5', 'Generate Test Data', create_sample_planes)
     menu.add_option('6', 'Exit Program', exit)
 
     return menu
 
-def delete_plane():
+def delete_bookmarked_plane(): # This is to delete a plane from the user db
     planes = db_manager.show_bookmarked_planes()
     for plane in planes:
         print(plane)
@@ -40,12 +40,12 @@ def delete_plane():
     db_manager.delete_plane_by_model_or_dbID(delete)
     pass
 
-def search_plane():
+def search_aircraft_in_api(): # This is to search for the plane in the api
     search = input('Enter plane model to search:')
     pass
 
 
-def plane_search():
+def search_bookmarked_plane(): # this searches in the user db
     search = input('Enter the plane you want to search for (Model or icao): ')
     plane = db_manager.search_plane(search)
     if plane is None:
@@ -53,12 +53,11 @@ def plane_search():
     else:
         print(plane)
 
-def bookmark_plane():
+def add_plane_to_bookmarks(): # This is to add a plane to the user db
     pass
 
 
-# Shows all bookmarked planes
-def get_show_bookmarked_planes():
+def display_bookmarked_planes(): # This displays all the planes in the user db
     planes = db_manager.show_bookmarked_planes()
     for plane in planes:
         print(plane)
@@ -67,16 +66,16 @@ def get_show_bookmarked_planes():
 # Developer test function just to create data to manipulate in the database. Should be removed once we can search for
 # actual plane data.
 def create_sample_planes():
-    boeing = Plane(name='Boeing 737', description='a plane created by Boeing.')
+    boeing = BookmarkedPlane(name='Boeing 737', description='a plane created by Boeing.')
     boeing.save()
-    airbus = Plane(name='Airbus A-321neo', description='a plane created by Airbus.')
+    airbus = BookmarkedPlane(name='Airbus A-321neo', description='a plane created by Airbus.')
     airbus.save()
 
 
 # Runs the program
 if __name__ == '__main__':
     api_url = 'http://api.aviationstack.com/v1/airplanes'
-    access_key = os.environ.get('AVIATIONS_API_KEY')
+    access_key = os.environ.get('AVIATIONS_API_KEY') # This is to get the api key from the environment variables
     
     if not os.path.exists('OneTimeMessage.txt'): # This is to check if the user has already been notified about the wait time for the api store the aircraft data into the db.
         
@@ -87,12 +86,11 @@ if __name__ == '__main__':
             file.write('True') # This will create a file to indicate that the user has already been notified about the wait time for the api to store the aircraft data into the db.
     print('')   
     print('')
-    db_manager = DatabaseManager(api_url, access_key)
+    db_manager = DatabaseManager(api_url, access_key) # This is to create the database manager object, with the api url and the access key.
     
     db_manager.setup_databases()  # This will set up your databases
     
     
-    # Check if the database is empty
-    if not UserPlane.select().exists() :
+    if not AircraftData.select().exists(): # This is to check if the aircraft data is already in the db. If it is then it won't fetch it again.
         db_manager.fetch_and_store_airplane_data() 
     Main()
--- databaseManager.py
+++ databaseManager.py
@@ -3,31 +3,30 @@ import os
 import requests
 
 # This is the database for the user's planes.
-db_1 = os.path.join('database', 'planes.sqlite')
-db_2 = os.path.join('database', 'aircraft.sqlite')
+bookmark_db_path = os.path.join('database', 'bookmarked_planes.sqlite')
+aircraft_data_db_path = os.path.join('database', 'aircraft_data.sqlite')
 
-userPlaneDB = SqliteDatabase(db_1)
-aircraftDB = SqliteDatabase(db_2)
+bookmarkedPlaneDB = SqliteDatabase(bookmark_db_path)   # This is the database for the user's planes they bookmark.
+aircraftDataDB = SqliteDatabase(aircraft_data_db_path)  # This is the database for the aircraft data, this is the data that is fetched from the API when you run the program for the first time.
 
-
-class Plane(Model):  # This is the model for the user's planes.
+class BookmarkedPlane(Model):  # This is the model for the user's planes.
     name = CharField()
     description = CharField()
 
     class Meta:
-        database = userPlaneDB
+        database = bookmarkedPlaneDB
 
     def __str__(self):
         return f'{self.name} is {self.description}'
 
 
-class UserPlane(Model):  # This is the model for the aircraft data.
+class AircraftData(Model):  # This is the model for the aircraft data.
     # This is the ICAO code for the aircraft. added null=True to allow for null values because some don't have an ICAO code.
-    icao = CharField(null=True)
+    icao = CharField(null=True) # some aircraft don't have an ICAO code so we need to allow for null values, so it doesn't stop the program
     model = CharField(null=True)
 
     class Meta:
-        database = aircraftDB
+        database = aircraftDataDB
 
     def __str__(self):
         return f'{self.model} and its icao code is {self.icao_code}'
@@ -35,26 +34,28 @@ class UserPlane(Model):  # This is the model for the aircraft data.
 
 class DatabaseManager:  # This is the class that manages the database operations
 
-    def __init__(self, api_url, access_key):
+    def __init__(self, api_url, access_key): # This is the constructor for the DatabaseManager class that takes in the api url and the access key.
         self.api_url = api_url
         self.access_key = access_key
 
-    def setup_databases(self):
-        os.makedirs('database', exist_ok=True)
-        aircraftDB.connect()
-        userPlaneDB.connect()
-        userPlaneDB.create_tables([Plane])
-        aircraftDB.create_tables([UserPlane])
+    def setup_databases(self): # This function sets up the databases
+        os.makedirs('database', exist_ok=True) # create the database folder if it doesn't exist already
+        bookmarkedPlaneDB.connect()
+        aircraftDataDB.connect()
+        bookmarkedPlaneDB.create_tables([BookmarkedPlane])
+        aircraftDataDB.create_tables([AircraftData])
+
 
     # this function fetches the data from the API and stores it in the database. Will break it up into smaller functions later.
     def fetch_and_store_airplane_data(self):
         try:
             response = requests.get(
-                f'{self.api_url}?access_key={self.access_key}&limit=10000')
+                f'{self.api_url}?access_key={self.access_key}&limit=10000') # This is the api url with the access key and the limit of 10000 to get a lot of data but the limit is a lot higher than 10k
             if response.status_code == 200:
-                airplane_data = response.json()['data']
+                airplane_data = response.json()['data'] 
                 for airplane in airplane_data:
-                    UserPlane.create(
+                    # this will not store the icao and model we get into aircraftDB
+                    AircraftData.create(
                         model=airplane['production_line'],
                         icao=airplane['iata_code_long']
                     )
@@ -65,13 +66,13 @@ class DatabaseManager:  # This is the class that manages the database operations
 
     # This function shows all the bookmarked planes
     def show_bookmarked_planes(self):
-        return Plane.select()
+        return BookmarkedPlane.select() 
 
     def create_plane(self, plane, description):
         # This checks to see if the plane already exists in the database.
-        existing_plane = Plane.get_or_none(Plane.name == plane)
-        if existing_plane is None:
-            plane = Plane(name=plane, description=description)
+        existing_plane = BookmarkedPlane.get_or_none(BookmarkedPlane.name == plane)
+        if existing_plane is None: # if the plane doesn't exist in the database then it will create it.
+            plane = BookmarkedPlane(name=plane, description=description)
             plane.save()
         else:
             print(f'Plane with name {plane} already exists.')
@@ -79,12 +80,12 @@ class DatabaseManager:  # This is the class that manages the database operations
     def delete_plane_by_model_or_dbID(self, model_or_dbID):
         try:
             dbID = int(model_or_dbID) # turn the string number into an integer
-            plane = Plane.get_or_none(
-                (Plane.id == dbID) | (Plane.name == model_or_dbID)) # now they can delete by the name or the database unique ID
+            plane = BookmarkedPlane.get_or_none(
+                (BookmarkedPlane.id == dbID) | (BookmarkedPlane.name == model_or_dbID)) # now they can delete by the name or the database unique ID
         except ValueError: # this will catch if they int and it will get plane by model name
-            plane = Plane.get_or_none(Plane.name == model_or_dbID)
+            plane = BookmarkedPlane.get_or_none(BookmarkedPlane.name == model_or_dbID)
 
-        if plane is not None:
+        if plane is not None: 
             plane.delete_instance()
             print(f'Successfully deleted plane: {model_or_dbID}.')
         else:
@@ -92,8 +93,9 @@ class DatabaseManager:  # This is the class that manages the database operations
 
     def search_plane(self, plane_name):
         # This searches for the plane in the database
-        plane = Plane.get_or_none(Plane.name == plane_name)
-        if plane is not None:
-            return str(plane)
+        plane = BookmarkedPlane.get_or_none(BookmarkedPlane.name == plane_name) 
+        # This checks to see if the plane exists in the database
+        if plane is not None: 
+            return str(plane) # This returns the plane as a string
         else:
             print(f'No plane found with name {plane_name}.')
