1535f415f3d7349dd812d6e0e8b34cef060ca8cc
ju957 <ju9572fp@go.minnstate.edu>
2023-10-25 14:47:55 -0500

    Add unit test

--- tests/flickr_tests.py
+++ tests/flickr_tests.py
@@ -14,10 +14,23 @@ class TestFlickrAPI(TestCase):
         with self.assertRaises(flickrapi.exceptions.FlickrError):
             get_image_link('B733')
 
+    """This test removes the Flickr API secret, which should cause an Exception and ask for authorization in a browser."""
+    @patch('planeImage.flickr_secret', 'No_secret')
+    def test_no_flickr_secret(self):
+        error_message = 'The Flickr API is currently unavailable.'
+        with self.assertRaises(flickrapi.exceptions.FlickrError):
+            get_image_link('B733')
+
     """This test inputs a search query that provides no images to manipulate, which should raise an IndexError."""
     def test_no_search_results(self):
         with self.assertRaises(IndexError):
             get_image_link('dfjhkvfsfjsvhsdsgkjh')
+
+    """This test checks what happens if an API connection isn't possible because of internet connection issues. The expectation is an error message
+    telling the user to fix their internet connection."""
+    def test_no_internet(self):
+        error_message = 'Error: No internet connection. Please fix your connection before using the Flickr API again.'
+        self.assertEqual(error_message, get_image_link('B773'))
 
 
 
